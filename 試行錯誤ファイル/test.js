8a3170d44ffeaf69eb153c9381c7ac42a112044d
nagito-hiroshima <68215637+nagito-hiroshima@users.noreply.github.com>
2021-05-13 23:40:25 +0900

    s

--- 試行錯誤ファイル/test.js
+++ 試行錯誤ファイル/test.js
@@ -1,11 +1,11 @@
 function run(){
-    var url = 'https://www.train-guide.westjr.co.jp/api/v3/sanyo2.json'
-    var url_st = url.replace('.json','_st.json')
-    res = request.urlopen(url)
-    res_st = request.urlopen(url_st)
-    data = json.loads(res.read().decode('utf-8'))
-    data_st = json.loads(res_st.read().decode('utf-8'))
-    dictst = {}
+    var url = 'https://www.train-guide.westjr.co.jp/api/v3/sanyo2.json';
+    var url_st = url.replace('.json','_st.json');
+    var res = request.urlopen(url);
+    var res_st = request.urlopen(url_st);
+    var data = json.loads(res.read().decode('utf-8'));
+    var data_st = json.loads(res_st.read().decode('utf-8'));
+    var dictst = {};
 
     for (station in data_st['stations']){
 
